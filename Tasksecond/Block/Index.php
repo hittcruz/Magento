6709ef76ee12713cc2e0b85b66acefb506cdc51b
Jonathan Lizama <jlizamal@emeal.nttdata.com>
2023-09-25 11:11:17 -0300

    cambios para el funcionamiento de vue.js

--- Tasksecond/Block/Index.php
+++ Tasksecond/Block/Index.php
@@ -2,5 +2,43 @@
 namespace Magento\Tasksecond\Block;
 
 class Index extends \Magento\Framework\View\Element\Template
+
 {
+
+    //protected $_alumnoFactory;
+
+    public function __construct(
+
+        \Magento\Framework\View\Element\Template\Context $context
+
+        //,
+
+        //\Modules\Alumnos\Model\AlumnoFactory $alumnoFactory //Aqui instancia toda la funcionalidad del metodo crud creado Post
+
+    )
+
+    {
+
+        //$this->_alumnoFactory = $alumnoFactory;
+
+        parent::__construct($context);
+
+    }
+
+    /*public function getAlumnoCollection(){
+
+        $alumno = $this->_alumnoFactory->create();
+
+        return $alumno->getCollection();
+
+    }*/
+
+ 
+
+    public function getFlag($flag){
+
+        return $flag;
+
+    }
+
 }
