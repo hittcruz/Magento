de361b1b5c6a53b7aeb7e67f0407b4ed9bcd64f4
mathichalla <mathias.munoz2016@alu.uct.cl>
2023-09-28 08:58:17 -0300

    Update App.js
    Se agregan funcionalidades del ciclo de vida

--- tarea/tema_task2/web/js/App.js
+++ tarea/tema_task2/web/js/App.js
@@ -1,16 +1,43 @@
-require(['vue', 'jquery', 'domReady!'], (Vue, $) => {
+require(["jquery", "domReady!", "vue"], function ($, doc, Vue) {
     const { createApp, ref } = Vue;
-
     const app = createApp({
-        setup() {
-            const greeting = ref('Hola Mundo desde Vue.js en Magento 2sssssssssssss');
-            const saludodd = ref("chao a todos xD");
-
+        data() {
             return {
-                greeting,
-                saludodd
+                respuesta: null,
+                holamundo: "Hola Mundo", // Define la constante "holamundo"
             };
-        }
-    }).mount('#app');
+        },
+        beforeCreate() {
+            console.log("beforeCreate: La instancia de Vue se está creando.");
+            // Puedes realizar configuraciones iniciales aquí.
+        },
+        created() {
+            console.log("created: La instancia de Vue se ha creado.");
+            // Puedes hacer llamadas a la API u otras inicializaciones aquí.
+        },
+        beforeUpdate() {
+            console.log(
+                "beforeUpdate: Los datos están a punto de actualizarse."
+            );
+        },
+        updated() {
+            console.log("updated: Los datos se han actualizado.");
+        },
+        mounted() {
+            const el = document.getElementById("app");
+            if (el) {
+                this.respuesta = el.getAttribute("data-valor");
+            }
+        },
+        methods: {
+            changeResp() {
+                console.log("presionando el botón cambiar");
+                if (this.respuesta === "OK") {
+                    this.respuesta = "NO";
+                } else {
+                    this.respuesta = "OK";
+                }
+            },
+        },
+    }).mount("#app");
 });
-
